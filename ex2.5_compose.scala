4620e00b615745db275bb3157cb5ceba9d40bbff
Apoorv Reddy <areddy@cognitivescale.com>
2016-04-12 17:27:50 +0530

    ex2.5 compose

--- ex2.5_compose.scala
+++ ex2.5_compose.scala
@@ -1,7 +1,7 @@
 object UtilFunctions {
 
     def compose[A, B, C](f: B => C, g: A => B): A => C =
-        (a: A) => f(g(a))
+        a => f(g(a))
 
     def main(args: Array[String]): Unit = {
         
